cae83ed92563f84b38b8a160362a1398caa39c7e
uoi100 <calvintruong11@hotmail.com>
2015-01-31 21:43:47 -0800

    Merge upstream/master into update/CalvinTruong Signed-off-by:uoi100 <calvintruong11@hotmail.com>

---
+++ application/core/Controller.php
@@ -0,0 +1,16 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Application extends CI_Controller
+{
+    protected $data = array();      // parameters for view components
+    protected $id;                  // identify for our content
+    protected $navbarChoices = array(
+        'Home' => '/', 
+    )
+}
---
+++ application/views/welcome.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <style type="text/css">
+        body{
+            background-color: #0059a6;
+        }
+    </style>
+</head>
+</html>
