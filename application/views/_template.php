bb6bf0048cdc53df24c0eacbc37e835307ad8ff9
uoi100 <calvintruong11@hotmail.com>
2015-02-12 16:51:13 -0800

    Implemented .css, changed the build_header_bar functionality so that the links appear on one line for the page. Signed-off-by:uoi100 <calvintruong11@hotmail.com>

--- application/views/_template.php
+++ application/views/_template.php
@@ -3,10 +3,7 @@
     <head>
         <title>{pagetitle}</title>
         <meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8"/>
-        <link rel="stylesheet" type="text/css" media="all" href="css/reset.css" />
-        <link rel="stylesheet" type="text/css" media="all" href="css/text.css" />
-        <link rel="stylesheet" type="text/css" media="all" href="css/style.css" />
-        <link rel="stylesheet" type="text/css" media="all" href="css/lightbox.css" />
+        <link rel="stylesheet" type="text/css" media="all" href="/assets/css/style.css" />
     </head>
     <body>
         <div id="wrapper">
