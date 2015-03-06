563f35a0a05f88390b30d69ccd29cc3dadde1fb4
uoi100 <calvintruong11@hotmail.com>
2015-03-05 16:56:51 -0800

    Minor Edit to CSS

--- application/views/admin.php
+++ application/views/admin.php
@@ -1,1 +1,9 @@
-Something goes here ;3
+This is the admin page. <BR/>
+
+In the admin page you are able to: <br/>
+
+<ol>
+    <li>Create a Blog</li>
+    <li>Edit a Blog</li>
+    <li>Delete a Blog</li>
+</ol>
